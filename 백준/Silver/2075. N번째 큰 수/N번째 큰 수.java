a54ab7c8a7e79fda3e484a550fda724734cfcce5
mmije0ng <127730905+mmije0ng@users.noreply.github.com>
2023-12-20 23:40:08 +0900

    [Silver II] Title: N번째 큰 수, Time: 888 ms, Memory: 177180 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,26 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+//최소힙을 이용한 풀이
 public class Main {
-	private static ArrayList<Integer> list;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine()); 
-		list=new ArrayList<>();
+		//최상위 최대값 n개를 minheap에 저장
+		PriorityQueue<Integer> minHeap=new PriorityQueue<Integer> ();
 		
 		StringTokenizer st;
 		for(int i=0;i<n;i++) {
 			st=new StringTokenizer(br.readLine()," ");
-			for(int j=0;j<n;j++) 
-				list.add(i*n+j,Integer.parseInt(st.nextToken()));
+			for(int j=0;j<n;j++) {
+				int num=Integer.parseInt(st.nextToken());
+				
+				if(minHeap.isEmpty() || minHeap.size()<n)
+					minHeap.offer(num);				
+				else {					
+					//원소가 현재 뽑힌 최상위 최대값의 가장 최소값보다 크다면 minHeap에 추가
+					if(num>minHeap.peek()) {
+						minHeap.poll();
+						minHeap.offer(num);
+					}
+				}
+			}
 		}
 			
-		Collections.sort(list, Collections.reverseOrder());
-		System.out.println(list.get(n-1));
+		//n번째로 큰 숫자는 최상위 최대값 n개를 저장해놓은 minHeap에서의 최솟값
+		System.out.println(minHeap.peek());
 	}
 }
