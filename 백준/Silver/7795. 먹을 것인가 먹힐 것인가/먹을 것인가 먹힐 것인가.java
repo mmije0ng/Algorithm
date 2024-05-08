c127c364e3e935f23870b4e16589812ce6b7b30f
박미정 <127730905+mmije0ng@users.noreply.github.com>
2024-05-08 17:19:03 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 640 ms, Memory: 46460 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// 이분탐색
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,11 +11,11 @@ public class Main {
         while(testCase--!=0){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            int aSize = Integer.parseInt(st.nextToken()); //A크기
-            int bSize = Integer.parseInt(st.nextToken()); //B 크기
+            int aSize = Integer.parseInt(st.nextToken()); //a크기
+            int bSize = Integer.parseInt(st.nextToken()); //b 크기
 
-            Integer[] aList = new Integer[aSize];
-            Integer[] bList = new Integer[bSize];
+            Integer[] aList = new Integer[aSize]; //a배열
+            Integer[] bList = new Integer[bSize]; //b배열
 
             st = new StringTokenizer(br.readLine());
             for(int i=0;i<aSize;i++) {
@@ -25,22 +26,27 @@ public class Main {
             for(int i=0;i<bSize;i++) {
                 bList[i] = Integer.parseInt(st.nextToken());
             }
-
-            Arrays.sort(aList,Collections.reverseOrder());
-            Arrays.sort(bList, Collections.reverseOrder());
+            Arrays.sort(bList);
 
             int cnt=0;
-            int bMin= bList[bSize-1];
 
             for(int i=0;i<aSize;i++){
-                if(aList[i]<=bMin)
-                    break;
-                for(int j=0;j<bSize;j++){
-                    if(aList[i]> bList[j]){
-                        cnt+=bSize-j;
-                        break;
-                    }
+                // 이분 탐색
+                int left=0;
+                int right=bSize-1;
+                int mid=0;
+                boolean isFind=false;
+
+                while(left<=right){
+                    mid=(left+right)/2;
+                    if(aList[i]>bList[mid])
+                        left=mid+1;
+                    // 탐색값이 bList[mid]값보다 작거나 같다면
+                    else
+                        right=mid-1;
                 }
+
+                cnt+=left;
             }
 
             bw.write(Integer.toString(cnt) + "\n");
