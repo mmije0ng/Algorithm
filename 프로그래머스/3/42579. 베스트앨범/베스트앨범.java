b2580e4cfcf3c78f9693c33391c3752aeda32a15
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-09-25 20:36:31 +0900

    Update 베스트앨범.java

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -4,16 +4,15 @@ import java.util.stream.Collectors;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> genreTotalMap = new HashMap<>(); // <장르, 총 재생 횟수>, { "classic" -> [0, 2, 3], "pop" -> [1, 4] }
-
-        
-        HashMap<String, ArrayList<Integer>> genreMap = new HashMap<>(); // <장르, 고유번호>
+        HashMap<String, Integer> genreTotalMap = new HashMap<>(); // <장르, 총 재생 횟수>, {pop=3100, classic=1450}
+        HashMap<String, ArrayList<Integer>> genreMap = new HashMap<>(); // <장르, 고유번호>, {pop=[4, 1], classic=[3, 0, 2]}
         
         for (int i = 0; i < genres.length; i++) {
             genreTotalMap.put(genres[i], genreTotalMap.getOrDefault(genres[i], 0) + plays[i]);
             genreMap.putIfAbsent(genres[i], new ArrayList<>());
             genreMap.get(genres[i]).add(i); // 고유번호(i)를 추가
         }
+        System.out.println(genreTotalMap);
         
         // 각 장르별 노래 정렬
         for (String genre : genreMap.keySet()) {
@@ -25,12 +24,15 @@ class Solution {
             });
         }
         
-        // 장르별 총합 정렬
+        // 장르별 총합 정렬 [pop, classic]
         List<String> sortedGenres = genreTotalMap.entrySet()
             .stream()
             .sorted((a, b) -> b.getValue().compareTo(a.getValue())) // 총 재생 횟수 내림차순
             .map(Map.Entry::getKey)
             .collect(Collectors.toList());
+        
+        System.out.println(sortedGenres);
+        System.out.println(genreMap);
 
         // 장르별 상위 2곡 추가
         for (String genre : sortedGenres) {
