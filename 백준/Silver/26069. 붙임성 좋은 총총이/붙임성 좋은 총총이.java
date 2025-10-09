f5d445dc9b04f043ca97684a4d2b2ddfcc782c24
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-10-09 15:51:21 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 124 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -1,38 +1,31 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String args[]) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 무지개 댄스를 추지 않고 있던 사람이 무지개 댄스를 추고 있던 사람을 만나게 된다면, 
-        // 만난 시점 이후로 무지개 댄스를 추게 된다
-        // 기록이 시작되기 이전 무지개 댄스를 추고 있는 사람은 총총이 뿐이라고 할 때, 
-        // 마지막 기록 이후 무지개 댄스를 추는 사람이 몇 명인지
-
-        int N = Integer.parseInt(br.readLine()); // 사람들이 만난 기록의 수
-        HashMap<String, String> map = new HashMap<>(); // <나(총총이가 된 사람), 나를 총총이로 만든>
-        map.put("ChongChong", "ChongChong");
-        int count = 0;
-
-        for(int i=0; i<N; i++){
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");  //총총이는 무조건 나오므로 추가
+        
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String me = st.hasMoreTokens() ? st.nextToken() : null;
-            String other = st.hasMoreTokens() ? st.nextToken() : null;
-
-            if(map.containsKey(other) || map.containsKey(me)){
-                map.put(me, other);
-                map.put(other, me);
-                // bw.write("me: " + me + "\n");
-                // bw.write("other: " + other + "\n");
+            String person1 = st.nextToken();
+            String person2 = st.nextToken();
+            
+            //춤을 추고 있는 사람을 만나면 춤을 추게 되므로 세트에 추가
+            if (set.contains(person1) || set.contains(person2)) {
+                set.add(person1);
+                set.add(person2);
             }
         }
-
-        bw.write(String.valueOf(map.size()) + "\n");
+        
+        bw.write(String.valueOf(set.size()));
         
         bw.flush();
         bw.close();
         br.close();
-  }
+    }
 }
