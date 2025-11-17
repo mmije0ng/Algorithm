1fa5ac9628e9bd6db457e03dec31c1dcf4903cb8
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-11-17 23:39:37 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        for(int i=0; i<commands.length; i++){
-            List<Integer> list = new ArrayList<>();
-            for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++){
-                list.add(array[j]);
-            }
-            Collections.sort(list);
-            answer[i] = list.get(commands[i][2]-1);
-        }      
-        
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1]; // copyOfRange는 end 인덱스 포함 X
+            int k = commands[i][2] - 1;
+
+            int[] temp = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(temp);
+
+            answer[i] = temp[k];
+        }
+
         return answer;
     }
 }
