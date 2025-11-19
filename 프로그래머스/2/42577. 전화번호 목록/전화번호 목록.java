1ec95921fe1541a15cce99dec989de32b2b74df9
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-11-19 13:07:57 +0900

    [level 2] Title: 전화번호 목록, Time: 252.96 ms, Memory: 190 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,18 +3,19 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i);
-        }
-        
-        // 각 번호의 접두어가 map에 있는지 확인
-        for (int i = 0; i < phone_book.length; i++) {
-            String number = phone_book[i];
-            for (int j = 0; j < number.length(); j++) { 
-                String prefix = number.substring(0, j);
-                if (map.containsKey(prefix)) {
+        Set<String> set = new HashSet<>();
+        for(String phone: phone_book)
+            set.add(phone);
+                
+        for(int i=0; i<phone_book.length; i++){
+            String phone = phone_book[i];
+            for(int j=0; j<phone.length(); j++){
+                String str = phone.substring(0, j);
+                
+                if(set.contains(str)){
+                    // System.out.println("phone: " + phone);
+                    // System.out.println("str: " + str);
+                    
                     answer = false;
                     return answer;
                 }
