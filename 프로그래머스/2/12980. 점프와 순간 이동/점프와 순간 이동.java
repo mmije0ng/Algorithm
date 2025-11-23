e7b0f8e54960e7c4d920e774b20d162ac9d89b8d
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-11-23 13:44:24 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,11 +1,15 @@
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-
-        while (n > 0) {
-            int remainder = n % 2;
-            n = n / 2; // 순간이동 (절반 이동)
-            ans += remainder; // 나머지가 1이면 점프한 것
+        
+        while(n > 0){
+            if(n%2==1){
+                // 1을 빼고 건전지 사용량을 1 더하기
+                n--;
+                ans++;
+            }
+            else
+                n/=2;
         }
 
         return ans;
