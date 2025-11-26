03c31b480380c24f7b1212a5f29839f096a1a701
박미정 <127730905+mmije0ng@users.noreply.github.com>
2025-11-26 11:15:39 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int size = brown + yellow;
+        int size = brown + yellow; // 전체 칸 개수
         
+        // 최소 세로 길이는 3 (테두리 각각 1줄 + 노란 칸 1줄)
         for(int rows = 3; rows < size/2; rows++){
-            if(size%rows == 0){
+            if(size%rows == 0){ // (rows × cols = 전체 크기)가 가능한 구조
                 int cols = size/rows;
                 
                 if(rows<cols) // 가로 길이는 세로 길이와 같거나, 세로 길이보다 길어야 함
                     continue;
                 
+                // 내부 노란 영역은 테두리를 제외한 부분
                 if ((rows - 2) * (cols - 2) == yellow) {
                     answer[0] = rows;
                     answer[1] = cols;
