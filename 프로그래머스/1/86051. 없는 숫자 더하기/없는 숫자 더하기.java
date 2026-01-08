097f4e526503ef3658271b7b231b7a968fcccac2
박미정 <arsvita0116@gmail.com>
2026-01-09 00:00:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for (int number : numbers)
+            set.add(number);
+        
+        for(int i=0; i<=9; i++){
+            if(!set.contains(i))
+                answer+=i;
+        }
+
+        return answer;
+    }
+}
